d23db637cc85a30fc3aea480dc117b4f28815aa9
Alumnos <pablocalderoncornejo@gmail.com>
2023-08-30 21:38:06 -0400

    Initial commit

---
+++ app/src/main/java/cl/ipvg/ejemploipvgsensores01/MainActivity.java
@@ -0,0 +1,38 @@
+package cl.ipvg.ejemploipvgsensores01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private SensorManager mgr;
+    private TextView textLista;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mgr=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        textLista =(TextView) findViewById(R.id.sensorsList);
+        List<Sensor> sensorList=mgr.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder stringBuilder= new StringBuilder();
+        for (Sensor s: sensorList){
+            stringBuilder.append(s.getName()+"\n");
+        }
+
+        textLista.setVisibility(View.VISIBLE);
+        textLista.setText(stringBuilder);
+
+
+    }
+}
